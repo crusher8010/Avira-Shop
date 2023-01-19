790790a99bad2310d7d35129093d188e70fc83b8
manojmogal007 <manojmogal07@gmail.com>
2023-01-19 18:58:51 +0530

    update signup and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,9 @@
 import Homepage from "./Components/Sudev/WholeHomepage/Homepage/Homepage"
-import Signup from './Components/Manoj/Signup'
-import Login from './Components/Manoj/Login'
-import AllRoutes from "./Pages/AllRoutes";
 
 function App() {
   return (
     <>
-      {/* <Homepage />  */}
-      {/* <Signup/>
-      <Login/> */}
-      <AllRoutes/>
+      <Homepage /> 
     </>
   );
 }
