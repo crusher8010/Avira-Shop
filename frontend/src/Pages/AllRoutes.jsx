9089b4fb73bb186d1463f80838913e0721ead328
crusher8010 <crusher8010@gmail.com>
2023-01-20 12:51:47 +0530

    added purrchase history page

--- frontend/src/Pages/AllRoutes.jsx
+++ frontend/src/Pages/AllRoutes.jsx
@@ -4,6 +4,7 @@ import Login from '../Components/Manoj/Login'
 import Signup from '../Components/Manoj/Signup'
 import AdminAddProducts from '../Components/Anirudha/AdminAddProducts';
 import AdminDashboard from '../Components/Anirudha/AdminDashboard';
+import PurchaseHistory from '../Components/Anirudha/PurchaseHistory';
 
 const AllRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AllRoutes = () => {
         <Route path='/login' element={<Login />} ></Route>
         <Route path='/adminDashboard' element={<AdminDashboard />}></Route>
         <Route path='/adminaddproduct' element={<AdminAddProducts />}></Route>
+        <Route path="/purchaseHistory" element={<PurchaseHistory />}></Route>
       </Routes>
     </div>
   )
