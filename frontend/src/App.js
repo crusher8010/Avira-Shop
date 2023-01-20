4f3cd85d31192c8ea7ee894b3f23a46c1caef6a6
crusher8010 <crusher8010@gmail.com>
2023-01-20 10:16:38 +0530

    corrected app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,8 @@
-
-
-import "./App.css";
-// import Homepage from "./Components/Sudev/WholeHomepage/Homepage/Homepage";
-import AdminDashboard from "./Components/Anirudha/AdminDashboard";
-import Western from "./Components/Ankita/western";
-
-import Homepage from "./Components/Sudev/WholeHomepage/Homepage/Homepage"
-
-
-
-function App() {
-  return (
-    <>
-
-      {/* <AdminDashboard />
-
-      <Homepage />  */}
-      <Western/>
-
 import Homepage from "./Components/Sudev/WholeHomepage/Homepage/Homepage"
 function App() {
   return (
     <>
-    <Homepage/>
+      <Homepage />
 
     </>
   );
