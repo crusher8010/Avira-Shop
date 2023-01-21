ca04904caf2bcf19536a87debfe728886803297a
crusher8010 <crusher8010@gmail.com>
2023-01-21 14:39:50 +0530

    added few changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,13 @@
+// import AllRoutes from "./Pages/AllRoutes";
 import Western from "./Components/Ankita/western";
-import Footer from "./Components/Sudev/WholeFooter/Footer/Footer";
-import Homepage from "./Components/Sudev/WholeHomepage/Homepage/Homepage"
-import LowerNavbar from "./Components/Sudev/WholeNavbar/LowerNavbar/LowerNavbar";
-import Navbar from "./Components/Sudev/WholeNavbar/MainNavbar/Navbar";
-import UpperNavbar from "./Components/Sudev/WholeNavbar/UpperNavbar/UpperNavbar";
+
 function App() {
   return (
     <>
       {/* <Navbar /> */}
-      <Homepage/>
-      {/* <Western/> */}
-      
+      {/* <AllRoutes /> */}
+      <Western />
+
     </>
   );
 }
